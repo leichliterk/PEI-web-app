84288882f7c04cdb55413dac02fce9022e281b08
leichliterk@gmail.com <leichliterk@gmail.com>
2025-06-19 18:13:25 -0500

    Update envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
+    production: false,
+    env_name: "STAGING",
+    app_version: "1.0.00",
+    API_SERVER: "https://sam-api-staging-nuzc.onrender.com/api/sam/",
     AUTH_SERVER: "https://sam24.us.auth0.com/dbconnections/",
     AUTH_CLIENT_ID: "yLMV6t5kbwlKjzDijVxHadty7yjhck1V",
 };
