0430010f56dc4ec0f6808f692ef422c74dbb2a91
leichliterk@gmail.com <leichliterk@gmail.com>
2025-09-06 16:00:24 -0500

    Add site service, update home page to display site info in cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SiteService, Site } from '../../services/site.service';
+import { SiteComponent } from '../site/site.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SiteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  sites: Site[] = [];
+
+  constructor(private siteService: SiteService) { }
+
+  ngOnInit(): void {
+    this.siteService.getAllSites().subscribe({
+      next: (sites) => {
+        console.log('Sites data:', sites);
+        this.sites = sites;
+      },
+      error: (error) => {
+        console.error('Error fetching sites:', error);
+      }
+    });
+  }
 
 }
---
+++ src/app/services/site.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface Site {
+  id: number;
+  name: string;
+  location?: string;
+  description?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllSites(): Observable<Site[]> {
+    return this.http.get<Site[]>(`${environment.API_SERVER}/site/getAllsites`);
+  }
+}
