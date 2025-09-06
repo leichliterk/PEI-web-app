3c15134a074377725960e46f3a2007cd923d0959
leichliterk@gmail.com <leichliterk@gmail.com>
2025-09-06 14:18:01 -0500

    Add home and site compoennts; add router-outlet and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SiteComponent } from './components/site/site.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'site', component: SiteComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
