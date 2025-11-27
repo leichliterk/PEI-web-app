1d9a8f4aca3126362df1fab691b284a83c12f779
Kent Leichliter <leichliterk@gmail.com>
2025-11-27 11:40:49 -0600

    Add Setting component, gear icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, ToolbarModule, ButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PEI-web-app';
+
+  constructor(private router: Router) {}
+
+  navigateToSettings() {
+    this.router.navigate(['/settings']);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SiteComponent } from './components/site/site.component';
 import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'site', component: SiteComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
