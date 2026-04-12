f24eedfddbd3fe604b6b42f904ab89b3aced28e8
Kent Leichliter <leichliterk@gmail.com>
2026-04-11 20:45:54 -0500

    CMM daily report rough-in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SiteComponent } from './components/site/site.component';
 import { HomeComponent } from './components/home/home.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { ReportsComponent } from './components/reports/reports.component';
 import { authGuard } from './guards/auth.guard';
 import { adminGuard } from './guards/admin.guard';
 
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'site/:id', component: SiteComponent, canActivate: [authGuard] },
   { path: 'settings', component: SettingsComponent, canActivate: [authGuard] },
   { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
+  { path: 'reports', component: ReportsComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
