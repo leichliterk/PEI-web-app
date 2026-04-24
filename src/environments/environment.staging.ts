c39a0febec0662743bc46d86e76b9e97e10db9e2
Kent Leichliter <leichliterk@gmail.com>
2026-04-24 10:01:00 -0500

    Add second status indicator to site card

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     env_name: "STAGING",
-    app_version: "1.0.0",
+    app_version: "1.4.0",
     API_SERVER: "https://pei-web-server-staging.onrender.com/api/data",
     WS_SERVER: "https://pei-web-server-staging.onrender.com",
     AUTH_DOMAIN: "sv-pei.us.auth0.com",
